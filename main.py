6429bec95f854d66b929c0d4edc013b1dd9fc55d
Tasina <1814184121@qq.com>
2025-03-02 00:38:41 +0800

    chore: 注释原神引用功能
    1. 暂时禁用 genshin_quote 方法
    2. 将原神引用语方法和相关代码注释掉
    3. 保留原有的引用语集合，以便后续可能的重新启用

--- main.py
+++ main.py
@@ -37,28 +37,28 @@ class MyQQBotPlugin(Star):
         ]
         yield event.chain_result(chain)
 
-    @llm_tool(name="genshin_quote")
-    async def genshin_quote(self, event: AstrMessageEvent) -> MessageEventResult:
-        """返回一句原神相关的话。
+    # @llm_tool(name="genshin_quote")
+    # async def genshin_quote(self, event: AstrMessageEvent) -> MessageEventResult:
+    #     """返回一句原神相关的话。
         
-        如果对方说了有关原神的话，AI 可以从中选取一句回复。
-        """
-        quotes = [
-            "「旅行者，你今天也要继续冒险吗？」",
-            "「风带来故事的种子，时间使之发芽。」", 
-            "「星星总在夜空闪耀，就像你的眼睛。」",
-            "「愿风神护佑你。」",
-            "「璃月的夜市可是出了名的热闹。」",
-            "「稻妻的樱花开得真美啊。」",
-            "「须弥的智慧之神在指引着我们。」",
-            "「枫丹的科技真是令人惊叹。」",
-            "「纳塔的沙漠中藏着无数秘密。」",
-            "「提瓦特大陆的每个角落都值得探索。」",
-            "「荧，我们一起去冒险吧！」",
-            "「派蒙永远是你最好的伙伴！」",
-            "「今天也是美好的冒险日和呢。」",
-            "「听说璃月港的海鲜很新鲜。」",
-            "「蒙德城的自由之风吹拂着大地。」"
-        ]
-        quote = random.choice(quotes)
-        yield event.plain_result(quote)
+    #     如果对方说了有关原神的话，AI 可以从中选取一句回复。
+    #     """
+    #     quotes = [
+    #         "「旅行者，你今天也要继续冒险吗？」",
+    #         "「风带来故事的种子，时间使之发芽。」", 
+    #         "「星星总在夜空闪耀，就像你的眼睛。」",
+    #         "「愿风神护佑你。」",
+    #         "「璃月的夜市可是出了名的热闹。」",
+    #         "「稻妻的樱花开得真美啊。」",
+    #         "「须弥的智慧之神在指引着我们。」",
+    #         "「枫丹的科技真是令人惊叹。」",
+    #         "「纳塔的沙漠中藏着无数秘密。」",
+    #         "「提瓦特大陆的每个角落都值得探索。」",
+    #         "「荧，我们一起去冒险吧！」",
+    #         "「派蒙永远是你最好的伙伴！」",
+    #         "「今天也是美好的冒险日和呢。」",
+    #         "「听说璃月港的海鲜很新鲜。」",
+    #         "「蒙德城的自由之风吹拂着大地。」"
+    #     ]
+    #     quote = random.choice(quotes)
+    #     yield event.plain_result(quote)
